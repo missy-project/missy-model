b7126857c2a7449ed3ea25a74a22471c1066a32c
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-03-27 09:47:23 +0100

    changed universeType relationship to manyToOne

--- src/main/java/org/gesis/missy/model/Universe.java
+++ src/main/java/org/gesis/missy/model/Universe.java
@@ -2,6 +2,7 @@ package org.gesis.missy.model;
 
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 @Entity( name = "MUniverse" )
@@ -12,7 +13,7 @@ public class Universe extends org.gesis.ddi.ontology.Universe
 
 	// relations
 
-	@OneToOne
+	@ManyToOne
 	@JoinColumn( name = "universeType_id" )
 	private UniverseType universeType;
 
@@ -22,24 +23,24 @@ public class Universe extends org.gesis.ddi.ontology.Universe
 
 	// getter/setter
 
-	public Universe( String agencyId, String objectId, int majorVersion )
+	public Universe( final String agencyId, final String objectId, final int majorVersion )
 	{
 		super( agencyId, objectId, majorVersion );
 	}
 
 	public UniverseType getUniverseType() {
-		return universeType;
+		return this.universeType;
 	}
 
-	public void setUniverseType(UniverseType universeType) {
+	public void setUniverseType(final UniverseType universeType) {
 		this.universeType = universeType;
 	}
 
 	public FilterInstruction getFilterInstruction() {
-		return filterInstruction;
+		return this.filterInstruction;
 	}
 
-	public void setFilterInstruction(FilterInstruction filterInstruction) {
+	public void setFilterInstruction(final FilterInstruction filterInstruction) {
 		this.filterInstruction = filterInstruction;
 	}
 
