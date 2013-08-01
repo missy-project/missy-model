60bc1985a6348a7fa7b64a14e970cc948f4bd2b4
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-08-01 17:13:19 +0200

    introduced ComputationBaseType as enum

--- src/main/java/org/gesis/missy/model/ComputationBaseType.java
+++ src/main/java/org/gesis/missy/model/ComputationBaseType.java
@@ -1,48 +1,9 @@
 package org.gesis.missy.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToOne;
 
-import org.gesis.persistence.PersistableResource;
-import org.gesis.rdf.LangString;
-
-@Entity( name = "Missy_ComputationBaseType" )
-public class ComputationBaseType extends PersistableResource
+public enum ComputationBaseType
 {
 
-	// properties
-
-	@Column
-	private String computationBaseType;
-
-	// relations
-
-	@OneToOne( cascade = CascadeType.ALL, fetch = FetchType.LAZY )
-	private LangString description;
-
-	// getter / setter
-
-	public LangString getDescription()
-	{
-		return description;
-	}
-
-	public String getComputationBaseType()
-	{
-		return computationBaseType;
-	}
-
-	public void setComputationBaseType( final String computationBaseType )
-	{
-		this.computationBaseType = computationBaseType;
-	}
-
-	public void setDescription( final LangString description )
-	{
-		this.description = description;
-	}
+	VALID_ONLY, MISSING_ONLY, TOTAL
 
 }
