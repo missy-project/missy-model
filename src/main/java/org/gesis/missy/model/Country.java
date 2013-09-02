ebf5636e3f49bf5756628a89881a0ff220290f39
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-09-02 15:30:48 +0200

    Country as own entity

---
+++ src/main/java/org/gesis/missy/model/Country.java
@@ -0,0 +1,48 @@
+package org.gesis.missy.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
+
+import org.gesis.persistence.PersistableResource;
+import org.gesis.rdf.LangString;
+
+@Entity( name = "Missy_Country" )
+public class Country extends PersistableResource
+{
+
+	// properties
+
+	@Column( length = 6 )
+	public String code;
+
+	// relations
+
+	@OneToOne( cascade = CascadeType.ALL, fetch = FetchType.LAZY )
+	public LangString prefLabel;
+
+	// getter / setter
+
+	public String getCode()
+	{
+		return code;
+	}
+
+	public void setCode( final String code )
+	{
+		this.code = code;
+	}
+
+	public LangString getPrefLabel()
+	{
+		return prefLabel;
+	}
+
+	public void setPrefLabel( final LangString prefLabel )
+	{
+		this.prefLabel = prefLabel;
+	}
+
+}
