f699afde2ad3c2d2d760b6fbee4eeb7624596b25
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-09-02 15:29:54 +0200

    removed ComputationBaseType; added Country relation

--- src/main/java/org/gesis/missy/model/CategoryStatistics.java
+++ src/main/java/org/gesis/missy/model/CategoryStatistics.java
@@ -1,32 +1,59 @@
 package org.gesis.missy.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity( name = "Missy_CategoryStatistics" )
 public class CategoryStatistics extends org.gesis.discovery.CategoryStatistics
 {
 	// properties
 
+	@Column
+	private float validPercentage;
+
+	@Column
+	private float totalPercentage;
+
 	// relations
 
-	@Enumerated( EnumType.STRING )
-	@Column
-	private ComputationBaseType computationBaseType;
+	@ManyToOne( cascade = CascadeType.ALL, fetch = FetchType.LAZY )
+	@JoinColumn( name = "country_id" )
+	private Country country;
 
 	// getter / setter
 
-	public ComputationBaseType getComputationBaseType()
+	public Country getCountry()
 	{
-		return computationBaseType;
+		return country;
 	}
 
-	public void setComputationBaseType( final ComputationBaseType computationBaseType )
+	public void setCountry( final Country country )
 	{
-		this.computationBaseType = computationBaseType;
+		this.country = country;
 	}
 
+	public float getValidPercentage()
+	{
+		return validPercentage;
+	}
+
+	public void setValidPercentage( final float validPercentage )
+	{
+		this.validPercentage = validPercentage;
+	}
+
+	public float getTotalPercentage()
+	{
+		return totalPercentage;
+	}
+
+	public void setTotalPercentage( final float totalPercentage )
+	{
+		this.totalPercentage = totalPercentage;
+	}
 
 }
